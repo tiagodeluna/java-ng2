77c628d08e870d96c422d6545ab3736eb7cbf4ca
Leonardo <leonardo.dias.oliveira@gmail.com>
2017-11-23 09:40:19 -0500

    Criado a estrutura dos projetos BackEnd e FrontEnd

---
+++ skipthedishes-api/src/main/java/com/skipthedishes/api/SkipthedishesApiApplication.java
@@ -0,0 +1,12 @@
+package com.skipthedishes.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkipthedishesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkipthedishesApiApplication.class, args);
+	}
+}
---
+++ skipthedishes-api/src/test/java/com/skipthedishes/api/SkipthedishesApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.skipthedishes.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SkipthedishesApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
