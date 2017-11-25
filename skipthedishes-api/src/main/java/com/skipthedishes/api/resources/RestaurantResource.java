ea4197402d91c2d6b8a5a797166344c1bc46c84f
Tiago Luna <tiagodeluna@hotmail.com>
2017-11-25 17:19:41 -0300

    Fed database with test data

--- skipthedishes-api/src/main/java/com/skipthedishes/api/resources/RestaurantResource.java
+++ skipthedishes-api/src/main/java/com/skipthedishes/api/resources/RestaurantResource.java
@@ -40,6 +40,12 @@ public class RestaurantResource {
         return ResponseEntity.status(HttpStatus.CREATED).body(restaurantList);
     }
 
+    @PutMapping(path = "bulk")
+    public ResponseEntity<List<Restaurant>> updateBulk(@RequestBody List<Restaurant> restaurantList) {
+        restaurantList.stream().forEach(r -> restaurantService.update(r.getId(), r));
+        return ResponseEntity.status(HttpStatus.CREATED).body(restaurantList);
+    }
+
     @PutMapping("/{id}")
     public ResponseEntity<Restaurant> update(@PathVariable String id, @Valid @RequestBody Restaurant restaurant) {
         return ResponseEntity.ok(restaurantService.update(id, restaurant));
