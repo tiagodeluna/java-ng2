d333db807d6ba5072341cff0b370adfef7dbc37d
Igor Harã Serafim <igorhara@gmail.com>
2017-11-25 03:02:35 +0100

    install of primeng and font-awesome.

--- skipthedishes-gui/src/app/app.component.ts
+++ skipthedishes-gui/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MenuItem} from "primeng/primeng";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  items: MenuItem[];
+
+  ngOnInit() {
+    this.items = [
+      {label: 'Search', icon: 'fa-plus'},
+      {label: 'Fav', icon: 'fa-download'},
+      {label: 'Orders', icon: 'fa-download'},
+      {label: 'Profile', icon: 'fa-refresh'}
+    ];
+  }
 }
--- skipthedishes-gui/src/app/app.module.ts
+++ skipthedishes-gui/src/app/app.module.ts
@@ -1,8 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from "./app.component";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {StepsModule} from "primeng/primeng";
 
 
 @NgModule({
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    StepsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
